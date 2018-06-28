34f5a04cdb7f3174a333b9ca10865122da0a01bf
katleiahramos <katleiahramos@gmail.com>
2018-06-28 14:49:43 -0500

    remove email attribute from users

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2018_06_28_160039) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
