878d7d548ad2deaf80f027217924627dfb6519e9
katleiahramos <katleiahramos@gmail.com>
2018-06-28 12:50:30 -0500

    add new and create routes in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   def new
     @user = User.new
-    
+
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    #authenticate pw
+    if @user && user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      #redirect to sign in page if authentificatin does not work
+      
+      redirect_to new_session_path
+    end
   end
 
   def destroy
