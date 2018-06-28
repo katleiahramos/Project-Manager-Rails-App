8180c23b4980238ce2c989b2ec2f572b0b50f4f7
katleiahramos <katleiahramos@gmail.com>
2018-06-28 13:01:59 -0500

    Add feature of creating session after user logs in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:user][:username])
     #authenticate pw
-    if @user && user.authenticate(params[:user][:password])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
       #redirect to sign in page if authentificatin does not work
-      
+
       redirect_to new_session_path
     end
   end
