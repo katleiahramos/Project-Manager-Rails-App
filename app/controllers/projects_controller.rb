a4cbf467313493b817d71ab8761525aed6d2f3e7
katleiahramos <katleiahramos@gmail.com>
2018-06-28 13:46:17 -0500

    add tasks to project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    @project = Project.find_by(params[:id])
+    @tasks = @project.tasks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :sessions
   resources :users, only: [:show]
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :show]
 end
