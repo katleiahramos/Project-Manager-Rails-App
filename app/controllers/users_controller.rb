d2168b03b2470c11a93837dbf11a5c46325223ed
katleiahramos <katleiahramos@gmail.com>
2018-06-28 14:05:47 -0500

    add create route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to controller: "users", action: "new"
+    end 
   end
 
   def show
     @user = User.find_by(params[:id])
     @tasks = @user.tasks
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
