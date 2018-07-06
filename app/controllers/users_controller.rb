90e70ec4e9acb5794ec2c8b35ef452643fed8669
katleiahramos <katleiahramos@gmail.com>
2018-07-05 17:02:24 -0700

    updated sign up route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if @user.save
 
       session[:user_id] = @user.id
-      redirect_to user_tasks_path(@user)
+      redirect_to projects_path
     else
       render :new, layout: false
     end
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
