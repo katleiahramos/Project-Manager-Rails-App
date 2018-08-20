583f7d38528510a09cf7ebcc2401ebf0e83dca78
katleiahramos <katleiahramos@gmail.com>
2018-08-20 11:19:04 -0500

    update minified js file

--- app/assets/javascripts/project_manager.js
+++ app/assets/javascripts/project_manager.js
@@ -13,13 +13,23 @@ const showTask = function (id) {
   // const taskID = id
   $.get(`/tasks/${id}` ,function(taskData){
     // {id: 2, due_date: "2018-07-12T00:00:00.000Z", description: "Create command line design", user_id: 3, project_id: 3, …}
-
     const dueDate = new Date(taskData.due_date)
     const formattedDueDate = dueDate.toDateString()
     const description = taskData.description
     const user = taskData.user.username
     $(`#task-${taskData.id}`).html( formattedDueDate + "<br>" + user )
+    // $(`#task-${taskData.id}`).html("TESTING")
 
 
   })
 }
+
+// $(function() {
+//   $('.task-more').click(function(e){
+//     e.preventDefault();
+//     var mymodal = $(``);
+//     mymodal.find('.modal-body').text('hello');
+//     mymodal.modal('show');
+//   })
+//
+// })
