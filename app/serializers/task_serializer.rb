0c1e222b42f98d958139908b4c9ee26a3e3f3028
katleiahramos <katleiahramos@gmail.com>
2018-08-21 14:22:27 -0500

    add name attribute to task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :description, :due_date
+  attributes :id, :description, :due_date, :name
   belongs_to :user
   belongs_to :project
 end
