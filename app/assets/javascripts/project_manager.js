e663ba4bcca7d326647b07fc3cedcb2a5b99a837
katleiahramos <katleiahramos@gmail.com>
2018-08-21 15:18:21 -0500

    clean up js on index page

--- app/assets/javascripts/project_manager.js
+++ app/assets/javascripts/project_manager.js
@@ -6,24 +6,43 @@ $(document).ready(function() {
 
 
 const attachListeners = function() {
+  $(function () {
+    $(".task-more").on("click", function(){
 
+      const id = $(this).data("id");
+      showTask(id);
+    })
+  })
+
+  $(function () {
+    $(".new-task").on("click", function(){
+      const projectId = $(this).data("project-id");
+      showTaskForm(projectId)
+    })
+  })
 }
 
 
 const showTask = function (id) {
   // debugger
   // const taskID = id
+
   $.get(`/tasks/${id}` ,function(taskData){
     // {id: 2, due_date: "2018-07-12T00:00:00.000Z", description: "Create command line design", user_id: 3, project_id: 3, …}
-    const dueDate = new Date(taskData.due_date)
-    const formattedDueDate = dueDate.toDateString()
+    let user = ""
+    let formattedDueDate = ""
+
+    if (taskData.due_date){
+      const dueDate = new Date(taskData.due_date)
+      formattedDueDate = dueDate.toDateString()
+    }
     const name = taskData.name
-    const user = taskData.user.username
+
+    if (taskData.user) { user = taskData.user.username}
 
     // const modal = $(`#task-${taskData.id}`)
     $('.modal-title').html(name)
-    $('.modal-body').html(formattedDueDate + "<br>" + user)
-
+    $('.modal-body').html('Due Date: ' + formattedDueDate + "<br> Assign to: " + user)
 
   })
 }
@@ -55,7 +74,11 @@ const showTaskForm = function(projectId) {
       const button = buttonizeTask(name, taskId);
 
       $(`#project-${projectId}`).find(".tasks").append(button);
-      // need to append modal
+      // add task listener here
+      $(".task-more").on("click", function(){
+        const id = $(this).data("id");
+        showTask(id);
+      })
       $(`#project-${projectId}`).find("form").get(0).reset()
       $(`#project-${projectId}`).find("form").find("input[type=submit]").removeAttr('disabled');
     })
@@ -66,40 +89,5 @@ const showTaskForm = function(projectId) {
 
 
 const buttonizeTask = function(name, taskId) {
-  return  `<button type="button" class="task-more btn btn-light my-2" data-id="${taskId}" name="button" data-toggle="modal" data-target="#task-${taskId}" >${name}</button>`
+  return  `<button type="button" class="task-more btn btn-light my-2" data-id="${taskId}" name="button" data-toggle="modal" data-target=".modal" >${name}</button>`
 }
-
-
-
-$(function() {
-  $(".taskForm").on("submit", function(event){
-
-    event.preventDefault();
-    const url = this.action
-    const values = $(this).serialize()
-
-    $.post(url, values).success(function(response){
-
-      const description = response.description;
-      const projectId = response.project.id
-      const taskId = response.id
-      const button = buttonizeTask(description, taskId);
-      $(`#project-${projectId}`).find(".tasks").append(button);
-
-      $(`#project-${projectId}`).find("form").get(0).reset()
-      $(`#project-${projectId}`).find("form").find("input[type=submit]").removeAttr('disabled');
-    })
-  })
-  // $("form").submit(function(event) {
-  //   event.preventDefault();
-  //   debugger
-  //   const values = $(this).serialize()
-  //
-  // })
-  // $("form").on('submit',function(event) {
-  //   event.preventDefault();
-  //
-  //
-  // })
-
-})
