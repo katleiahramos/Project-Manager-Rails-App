53bc0ef12661e4f7b743513d1d446e8bad977896
katleiahramos <katleiahramos@gmail.com>
2018-08-31 11:48:11 -0500

    add completed as task attr in seralizer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :description, :due_date, :name
+  attributes :id, :description, :due_date, :name, :completed
   belongs_to :user
   belongs_to :project
 end
